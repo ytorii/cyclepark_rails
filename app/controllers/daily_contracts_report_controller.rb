21a830fe84c67c4704acfb333104bf7a36db198d
YTorii <metalwarrior_type_jp@hotmail.com>
2016-08-31 12:20:22 +0900

    Refactored daily and count controller with Rubocop

--- app/controllers/daily_contracts_report_controller.rb
+++ app/controllers/daily_contracts_report_controller.rb
@@ -1,16 +1,34 @@
+# Controller for listing DailyContractsReport
 class DailyContractsReportController < ApplicationController
   def index
-    report = DailyContractsReport.new(params[:contracts_date])
-    if report.valid?
-      @contracts_date  = report.contracts_date
-      @contracts_list  = report.contracts_list
-      @contracts_total = report.contracts_total
+    @report = DailyContractsReport.new(report_params)
+
+    if @report.valid?
+      result_parameters
     else
-      respond_to do |format|
-        format.html {
-          redirect_to menu_path,
-          alert: report.errors.full_messages
-        }
+      error_respond
+    end
+  end
+
+  private
+
+  # strong parameters
+  def report_params
+    if params[:contracts_date]
+      params.permit(:contracts_date).require(:contracts_date)
+    end
+  end
+
+  def result_parameters
+    @contracts_date  = @report.contracts_date
+    @contracts_list  = @report.contracts_list
+    @contracts_total = @report.contracts_total
+  end
+
+  def error_respond
+    respond_to do |format|
+      format.html do
+        redirect_to menu_path, alert: @report.errors.full_messages
       end
     end
   end
