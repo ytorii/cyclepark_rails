b6a43e227cb9994e3c26fe49147148d607b8b93e
ytorii <metalwarrior_type_jp@hotmail.com>
2017-08-10 18:05:41 +0900

    Add staffdetail to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Staff.create(nickname: "admin", password:"12345678", admin_flag: true)
+Staff.create(
+  nickname: "admin",
+  password:"12345678",
+  admin_flag: true,
+  staffdetail_attributes: {
+    name: "管理者　ユーザ",
+    read: "かんりしゃ　ゆーざ",
+    address: "寝屋川市八坂町１７－１",
+    birthday: '1978/01/01',
+    phone_number: "000-777-8888",
+    cell_number: "090-0000-1111"
+  }
+)
